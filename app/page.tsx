25452530071a48668f2633207927694cfb1b29ac
Bastian Scharnagl <bastian.scharnagl@gmail.com>
2025-07-15 08:11:10 +0200

    swc and webpack

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div id="menu">
       <nav>
       <ol>
-      <li><a href="/">Wurzel </a></li>
+      <li><a href="/">Wurzeln </a></li>
       <li><a href="https://linkedin.com/in/bastianscharnagl">Lebenslauf </a></li>
       <li><a href="/contact">BS-3PO </a></li>
       </ol>
@@ -17,7 +17,7 @@ export default function Home() {
 
       <div className='outer-div'>
         <div className='middle-div'>
-          <p className="align-middle text-lg italic">Formale Logik ist unabhängig von Erfahrung</p>
+          <p className="align-middle text-lg italic">Erfahrung, unabhängig von formaler Logik</p>
         </div>
       </div>
     </main>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone",
+  swcMinify: true,
+  webpack: (config) => {
+    config.snapshot = {
+      ...(config.snapshot ?? {}),
+      // Add all node_modules but @next module to managedPaths
+      // Allows for hot refresh of changes to @next module
+      managedPaths: [/^(.+?[\\/]node_modules[\\/])(?!@next)/],
+    };
+    return config;
+  },
   rewrites: async () => {
     return [
       {
